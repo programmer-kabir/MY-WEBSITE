e51717ad997ce1c4e2eea0226cdeea216d54123b
developerremran <developerremran@gmail.com>
2023-10-14 10:34:09 +0600

    problem admin dashboard

---
+++ Client/src/Pages/Dashboard/Add-Tamplate/AddTemplate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddTemplate = () => {
+    return (
+        <div>
+            <h2>This is a Admin template</h2>
+        </div>
+    );
+};
+
+export default AddTemplate;
---
+++ Client/src/Pages/Dashboard/ComponentsList/ComponentsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComponentsList = () => {
+    return (
+        <div>
+           <h2>THis is a components list</h2> 
+        </div>
+    );
+};
+
+export default ComponentsList;
